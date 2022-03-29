ab31845fcbafa2050910d2cc527e8a8f8feca46c
yeeun0702 <101261625+yeeun0702@users.noreply.github.com>
2022-03-29 16:10:53 +0900

    Create 2-13.c

---
+++ 2-13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main() {
+    // 1번을 해보세요!
+    double force_a; 
+    double force_b; 
+    double ratio;
+    // 2번을 해보세요!
+    force_a = 10.5*8.4; 
+    force_b = 12.2*6.3; 
+    // 3번을 해보세요!
+    ratio = force_a/force_b;
+
+    printf("a 포탄의 힘은 %f, b 포탄의 힘은 %f입니다.\n", force_a, force_b);
+    printf("a 포탄이 b 포탄보다 %f배 더 셉니다.\n", ratio);
+}
+
