99e1e0076c6dd925d7eafa40d58f118eddd1702d
yeeun0702 <101261625+yeeun0702@users.noreply.github.com>
2022-04-01 23:55:28 +0900

    Create 3-6.c

---
+++ 3-6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void test3(void); // ① 함수 선언 test3()
+
+int main()
+{
+    test3();
+    test3();
+    test3();
+
+    return 0;
+}
+// 1번을 해보세요!
+    void test3()
+{ 
+    printf("Hello\n");
+}
