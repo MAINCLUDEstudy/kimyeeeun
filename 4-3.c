8e59d9ebe79ae4df9fbdf2f5215e077a31109c59
yeeun0702 <101261625+yeeun0702@users.noreply.github.com>
2022-04-04 18:02:52 +0900

    Create 4-3.c

---
+++ 4-3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1, b;
+    // 1번을 해보세요!
+    b=++a;
+    
+    printf("a의 값은 %d\n", a);
+    printf("b의 값은 %d\n", b);
+    printf("\n");
+    
+    a = 1;
+    // 2번을 해보세요!
+    b=a++;
+    
+    printf("a의 값은 %d\n", a);
+    printf("b의 값은 %d\n", b);
+
+    return 0;
+}
+
+
