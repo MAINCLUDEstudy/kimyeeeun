248b4ebf8d358cd4444fa24e96534329fcb65226
yeeun0702 <101261625+yeeun0702@users.noreply.github.com>
2022-04-04 18:03:24 +0900

    Create 4-4.c

---
+++ 4-4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 2;
+    // 1번을 해보세요!
+    printf("a 더하기 b = %d\n", a + b);
+    // 2번을 해보세요!
+    printf("a 빼기   b = %d\n", a - b);
+    // 3번을 해보세요!
+    printf("a 곱하기 b = %d\n", a * b);
+    // 4번을 해보세요!
+    printf("a 나누기 b = %d\n", a / b);
+    // 5번을 해보세요!
+    printf("a 나머지 b = %d\n", a % b);
+
+    return 0;
+}
