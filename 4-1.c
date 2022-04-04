65ec818abf8c20352985af9c449db8a3918c1d13
yeeun0702 <101261625+yeeun0702@users.noreply.github.com>
2022-04-04 17:48:50 +0900

    Create 4-1.c

---
+++ 4-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    c = 10;
+    // 1번을 해보세요!
+    a=b=c;
+
+    printf("a의 값은 %d\n", a);
+    printf("b의 값은 %d\n", b);
+    printf("c의 값은 %d\n", c);
+    
+    return 0;
+}
+
