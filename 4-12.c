c7ff2f70474f6c893f040578fb111a921ce0bb57
yeeun0702 <101261625+yeeun0702@users.noreply.github.com>
2022-04-09 18:01:52 +0900

    Create 4-12.c

---
+++ 4-12.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a = 178;
+    int b = 104;
+    int c = 10;
+    // 1번을 해보세요!
+    printf("%d & %d = %d\n", a, b, a & b);
+    // 2번을 해보세요!
+    printf("%d | %d = %d\n", a, b,a | b);
+    // 3번을 해보세요!
+    printf("%d ^ %d = %d\n", a, b,a ^ b);
+    // 4번을 해보세요!
+    printf("c = %d, ~c = %d\n", c, ~c);
+
+    return 0;
+}
+
+
