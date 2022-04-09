76f7c040f07998476ba942c3fd205b12b0a3d161
yeeun0702 <101261625+yeeun0702@users.noreply.github.com>
2022-04-09 18:00:59 +0900

    Create 4-11.c

---
+++ 4-11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // 1번을 해보세요!
+    if ( -3.3)
+        printf("TRUE 1\n");
+    // 2번을 해보세요!
+    if (3 )
+        printf("TRUE 2\n");
+    // 3번을 해보세요!
+    if ( 1)
+        printf("TRUE 3\n");
+    // 4번을 해보세요!
+    if ( 0)
+        printf("TRUE 4\n");
+
+    return 0;
+}
