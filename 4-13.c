3d987bfb9065e87ef4a7cbadaa539229ce7a544d
yeeun0702 <101261625+yeeun0702@users.noreply.github.com>
2022-04-09 18:02:26 +0900

    Create 4-13.c

---
+++ 4-13.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x = 4;
+    int n = 2;
+    // 1번을 해보세요!
+    printf("%d << %d = %d\n",x,n, x << n);
+    // 2번을 해보세요!
+    printf("%d >> %d = %d\n",x,n, x >> n);
+
+    return 0;
+}
