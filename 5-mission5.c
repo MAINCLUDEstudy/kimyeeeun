b13c8eb77e24fb35a5d883c0fbbca428e1cfea10
yeeun0702 <101261625+yeeun0702@users.noreply.github.com>
2022-04-12 17:21:09 +0900

    Create 5-mission5.c

---
+++ 5-mission5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+
+    printf("하나의 정수를 입력하세요:");
+    scanf("%d", &a);
+    // 1번을 해보세요!
+    if(a % 2)
+    printf("홀수입니다.\n");
+    else
+    printf("짝수입니다.\n");
+    
+    
+
+    return 0;
+}
