e7fc150df712226fce4d4003ff57c3bea68ce569
yeeun0702 <101261625+yeeun0702@users.noreply.github.com>
2022-04-12 21:37:57 +0900

    Create 5-mission6.c

---
+++ 5-mission6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+
+    printf("하나의 정수를 입력하세요: ");
+    scanf("%d", &a);
+    //1번을 해보세요!
+    if (a == 0)
+    printf("입력된 수는 0입니다.\n");
+    
+    else 
+    a>0?printf("입력된 수는 양수입니다.\n"):printf("입력된 수는 음수입니다.\n");
+    
+    
+    
+
+return 0;
+
+}
