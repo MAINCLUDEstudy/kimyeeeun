686eb13213dcf9e30d1c31737e6958a0af28dfc2
yeeun0702 <101261625+yeeun0702@users.noreply.github.com>
2022-04-12 17:19:33 +0900

    Create 5-1.c

---
+++ 5-1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 3;
+    // 1번을 해보세요!
+    if (5 > 3)
+    printf("A\n");
+    printf("B\n");
+    // 2번을 해보세요!
+    if (5 + 3) ;
+    printf("C\n");
+    printf("D\n");
+    // 3번을 해보세요!
+    if (5 - 5);
+    printf("F\n");
+    // 4번을 해보세요!
+    if (a > b);
+    printf("G\n");
+    // 5번을 해보세요!
+    if (a > 3);
+    printf("I\n");
+    
+    return 0;
+}
