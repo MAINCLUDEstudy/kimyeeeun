f497f2039182f293a3db65a23221811010c4c554
yeeun0702 <101261625+yeeun0702@users.noreply.github.com>
2022-04-12 17:20:03 +0900

    Create 5-2.c

---
+++ 5-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 1번을 해보세요!
+    if(5 > 3)
+    printf("A\n");
+    printf("B\n");
+    
+    printf("C\n");
+    // 2번을 해보세요!
+    if(5 < 3)
+    printf("D\n");
+    printf("E\n");
+    
+    
+    
+    printf("F\n");
+    
+    return 0;
+}
